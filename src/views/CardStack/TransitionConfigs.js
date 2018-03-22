bfee4ad01a1d9e8791c0ee16e378f4869d2465bc
Wassim Gharbi <wassgha@gmail.com>
2018-03-22 01:59:34 -0400

    Same transitions as iOS for android

--- src/views/CardStack/TransitionConfigs.js
+++ src/views/CardStack/TransitionConfigs.js
@@ -38,28 +38,6 @@ const ModalSlideFromBottomIOS = {
   },
 };
 
-// Standard Android navigation transition when opening an Activity
-const FadeInFromBottomAndroid = {
-  // See http://androidxref.com/7.1.1_r6/xref/frameworks/base/core/res/res/anim/activity_open_enter.xml
-  transitionSpec: {
-    duration: 350,
-    easing: Easing.out(Easing.poly(5)), // decelerate
-    timing: Animated.timing,
-  },
-  screenInterpolator: CardStackStyleInterpolator.forFadeFromBottomAndroid,
-};
-
-// Standard Android navigation transition when closing an Activity
-const FadeOutToBottomAndroid = {
-  // See http://androidxref.com/7.1.1_r6/xref/frameworks/base/core/res/res/anim/activity_close_exit.xml
-  transitionSpec: {
-    duration: 230,
-    easing: Easing.in(Easing.poly(4)), // accelerate
-    timing: Animated.timing,
-  },
-  screenInterpolator: CardStackStyleInterpolator.forFadeFromBottomAndroid,
-};
-
 function defaultTransitionConfig(
   // props for the new screen
   transitionProps,
@@ -68,18 +46,6 @@ function defaultTransitionConfig(
   // whether we're animating in/out a modal screen
   isModal
 ) {
-  if (Platform.OS === 'android') {
-    // Use the default Android animation no matter if the screen is a modal.
-    // Android doesn't have full-screen modals like iOS does, it has dialogs.
-    if (
-      prevTransitionProps &&
-      transitionProps.index < prevTransitionProps.index
-    ) {
-      // Navigating back to the previous screen
-      return FadeOutToBottomAndroid;
-    }
-    return FadeInFromBottomAndroid;
-  }
   // iOS and other platforms
   if (isModal) {
     return ModalSlideFromBottomIOS;
